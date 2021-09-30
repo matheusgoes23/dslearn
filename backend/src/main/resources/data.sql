69663512dd17fc10a60c5ad01a231019108e50d3
matheusgoes23 <matheusgoes23@gmail.com>
2021-09-30 15:01:47 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequesite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJUTCHW1KnIfex_vast7SA3xiK2J5P3jTC3A&usqp=CAU', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequesite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJUTCHW1KnIfex_vast7SA3xiK2J5P3jTC3A&usqp=CAU', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequesite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJUTCHW1KnIfex_vast7SA3xiK2J5P3jTC3A&usqp=CAU', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
